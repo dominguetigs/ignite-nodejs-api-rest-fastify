e7ca171d86e3ca63f63ad453c772de3783cda870
Gustavo Domingueti <gustavo.domingueti@skeelo.app>
2023-06-01 20:53:39 -0300

    chore: production config

--- src/database.ts
+++ src/database.ts
@@ -2,11 +2,16 @@ import { knex as setupeKnex, Knex } from 'knex'
 
 import { env } from './env'
 
+const connection =
+  env.DATABASE_CLIENT === 'sqlite'
+    ? {
+        filename: env.DATABASE_URL,
+      }
+    : env.DATABASE_URL
+
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection,
   // This config is because sqlite doesn't insert default values
   useNullAsDefault: true,
   migrations: {
